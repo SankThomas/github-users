de66c07efb9c61498542ceb07b9978f91d4ba93f
Thomas Sankara <thomassankara74@gmail.com>
2022-06-02 14:51:07 +0300

    run GET request + render Profile and Loading states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import Loading from "./Loading"
+import Profile from "./Profile"
+
+// const singleUser = `https://api.github.com/users/SankThomas`
+// const repos = `https://api.github.com/users/sankthomas/repos?per_page=5`
+// https://api.github.com/users/sankthomas/repos?page=1&per_page=10&sort=updated
 
 function App() {
+  const [items, setItems] = useState([])
+  // Change this to any username whose repositories you want to get
+  const [user] = useState("sankthomas")
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      const res = await fetch(
+        `https://api.github.com/users/${user}/repos?per_page=6&sort=updated`
+      )
+      const data = await res.json()
+      setItems(data)
+      console.log(data)
+    }
+
+    fetchRepos()
+  }, [user])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="pt-10">
+        <h1 className="mb-10 font-bold text-2xl">
+          Viewing {user}'s repositories
+        </h1>
+      </div>
+
+      {!items ? (
+        <Loading />
+      ) : (
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 pb-20">
+          {items.map((item) => (
+            <Profile key={item.id} {...item} />
+          ))}
+        </div>
+      )}
+    </>
+  )
 }
 
-export default App;
+export default App
